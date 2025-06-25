b08849d9e3c1807e24e6f63e023d0e55003497b8
joshdevv0106 <joshdevv0106@gmail.com>
2025-06-25 14:17:50 +0100

    initial commit

---
+++ product_access_controls/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Powered by Mindphin.
+# © 2023 Mindphin. (<https://www.mindphin.com>).
+
+{
+    'name': "Product Access controls",
+    "summary": """Product Access Control – Secure Product Fields in Odoo.
+    Inventory | Inventory controls | Product customization | Product details.
+    """,
+    "description": """Restrict access to sensitive product fields like cost price and product type in Odoo. Ideal for finance, procurement, and inventory security.
+    """,
+    "category": "Point of Sale",
+    "version": "18.0.1.0",
+    'author': 'SageOre',
+    'support': 'joshdevv0106@gmail.com',
+    'depends': ['stock', 'product', 'sale'],
+    'data': [
+        'security/security.xml',
+        'views/product.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
